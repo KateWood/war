14c9b9193e7ff43285cb31fddb99a64a73d18515
Kate Wood <katewood611@gmail.com>
2017-01-12 21:56:17 -0800

    adds basic win logic

--- js/app.js
+++ js/app.js
@@ -46,11 +46,22 @@ newDeck()
 function draw(player) {
 	var i = Math.floor(Math.random() * deck.length)
 	var choice = deck.splice(i, 1)
-	player.deck.push(choice)
+	player.deck.unshift(choice[0])
 	console.log(player.name + ' plays ' + choice[0].declare())
 }
 
 function play() {
 	draw(player1)
 	draw(player2)
+	calculateWin()
+}
+
+function calculateWin() {
+	if (vals.indexOf(player1.deck[0].value) > vals.indexOf(player2.deck[0].value)) {
+		console.log(player1.name + ' wins with a(n) ' + player1.deck[0].value)
+	} else if (vals.indexOf(player2.deck[0].value) > vals.indexOf(player1.deck[0].value)) {
+		console.log(player2.name + ' wins with a(n) ' + player2.deck[0].value)
+	} else if (vals.indexOf(player1.deck[0].value) === vals.indexOf(player2.deck[0].value)) {
+		console.log('WAR!')
+	}
 }
