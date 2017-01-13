2cde54cc17c0c9eeaa83a7e2f6f4a8ffb4aeccbf
Kate Wood <katewood611@gmail.com>
2017-01-12 20:00:23 -0800

    constructs deck

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 console.log('connected')
+
+function Card(suit, value) {
+	this.suit = suit
+	this.value = value
+	this.declare = function() {
+		return (value + ' of ' + suit)
+	}
+}
+
+var suits = ['spades', 'hearts', 'clubs', 'diamonds']
+var vals = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+var deck = []
+
+for(var i = 0; i < suits.length; i++) {
+	for(var j = 0; j < vals.length; j++) {
+		deck.push(new Card(suits[i], vals[j]))
+	}
+}
+
+console.log(deck)
