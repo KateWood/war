beb3089dcd2dcfdfc944e140bcae051763e987b1
Kate Wood <katewood611@gmail.com>
2017-01-12 21:43:28 -0800

    adds play function

--- js/app.js
+++ js/app.js
@@ -2,18 +2,26 @@ console.log('connected')
 
 // Create players
 
-user1 = prompt('Player 1, please enter your name')
-player1 = new Player(user1)
-user2 = prompt('Player 2, please enter your name')
-player2 = new Player(user2)
+function getPlayerNames() {
+	user1 = prompt('Player 1, please enter your name')
+	player1 = new Player(user1)
+	user2 = prompt('Player 2, please enter your name')
+	player2 = new Player(user2)
 
-function Player(name) {
-	this.name = name
-	this.score = 0
-	this.deck = []
+	function Player(name) {
+		this.name = name
+		this.score = 0
+		this.deck = []
+	}
 }
 
+getPlayerNames()
+
 // Build deck of cards
+
+var suits = ['spades', 'hearts', 'clubs', 'diamonds']
+var vals = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+var deck = []
 
 function Card(suit, value) {
 	this.suit = suit
@@ -23,23 +31,26 @@ function Card(suit, value) {
 	}
 }
 
-var suits = ['spades', 'hearts', 'clubs', 'diamonds']
-var vals = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
-var deck = []
-
-for(var i = 0; i < suits.length; i++) {
-	for(var j = 0; j < vals.length; j++) {
-		deck.push(new Card(suits[i], vals[j]))
+function newDeck() {
+	for(var i = 0; i < suits.length; i++) {
+		for(var j = 0; j < vals.length; j++) {
+			deck.push(new Card(suits[i], vals[j]))
+		}
 	}
 }
 
+newDeck()
 
+// Game Play
 
-
-
-
-function draw() {
+function draw(player) {
 	var i = Math.floor(Math.random() * deck.length)
 	var choice = deck.splice(i, 1)
-	console.log(choice[0].declare())
+	player.deck.push(choice)
+	console.log(player.name + ' plays ' + choice[0].declare())
+}
+
+function play() {
+	draw(player1)
+	draw(player2)
 }
